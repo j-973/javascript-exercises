8ffbfbc3c37c9b12b748ffc8caaf833291b64ff6
j-973 <jvoorheis973@gmail.com>
2023-02-02 13:48:53 -0500

    solution for javascript excersise 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,10 @@
-const repeatString = function() {
+//USING ES6 repeat() method
+const repeatString = function(string, num) {
+   if (num >= 0) { return string.repeat(num); }
+    else return 'ERROR';
+}
 
-};
+repeatString('', -1);
 
 // Do not edit below this line
 module.exports = repeatString;
