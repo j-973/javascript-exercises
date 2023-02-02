3feec2619444469ddc0f848ae031554181867652
j-973 <jvoorheis973@gmail.com>
2023-02-02 14:06:00 -0500

    solution for javascript excersise 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    //splits every character of a string into a new array (because empty string "" is the separator parameter)
+    //then these single-character arrays have their order reversed
+    //finally the join method concatenates these single-character arrays on to every other single-character array, returning a whole, reversed string
+    return string.split("").reverse().join("");
 };
 
 // Do not edit below this line
