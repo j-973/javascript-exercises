3b91b5f5a9e40241badb1a3cee5c7b0f5d5db508
j-973 <jvoorheis973@gmail.com>
2023-03-21 18:50:36 -0400

    added 08_calculator solution

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,33 @@
-const add = function() {
-	
+const add = function(x, y) {
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+  return x - y;
 };
 
-const sum = function() {
-	
+//initial value of arr.reduce set to 0
+const sum = function(arr) {
+  return arr.reduce((runningTotal, currentVal) => { return runningTotal + currentVal; }, 0);
 };
 
-const multiply = function() {
-
+//initial value of arr.reduce set to 1 instead of 0 for the initial value since anything times 0 is 0
+const multiply = function(arr) {
+  return arr.reduce((runningTotal, currentVal) => { return runningTotal * currentVal; }, 1);
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+  return Math.pow(num, power);
 };
 
-const factorial = function() {
-	
-};
+function factorial(num) {
+  let result = 1;
+  for (let i = num; i > 0; i--) {
+      // Multiply the result every time i counts down from the starting number
+      result *= i;
+  }
+  return result;
+}
 
 // Do not edit below this line
 module.exports = {
