2707cab52ecede2bc37001f9552eab3e36358b52
j-973 <jvoorheis973@gmail.com>
2023-04-13 15:41:03 -0400

    submitted solution exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,10 @@
-const removeFromArray = function() {
-
+//2 parameters: arr is the array where values will be removed
+// ...values uses rest parameter syntax that takes an indefinite number of values to be removed
+// The filter method makes a new array that includes all of the items not specified as those to be removed from the second parameter ...values
+const removeFromArray = function(arr, ...values) {
+    return arr.filter(item => !values.includes(item));
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
