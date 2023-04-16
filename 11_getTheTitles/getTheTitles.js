5ebb5f773acd2c6009ac59550a9d6ef32493d5ce
j-973 <jvoorheis973@gmail.com>
2023-04-16 17:27:08 -0400

    submitted solution exercise 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+//in this case, the .map function gets the title of each book from the inputted books array, and makes a new array of those titles, called bookTitles
+const getTheTitles = function(arr) {
+    const bookTitles = arr.map(book => book.title);
+    return bookTitles;
 };
 
 // Do not edit below this line
